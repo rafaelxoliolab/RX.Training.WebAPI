07b2a714c90127ebb9eb914cdb29d1f2e7acba81
Rafael Xolio <rafael_xolio@epamneoris.com>
2026-03-02 19:13:26 -0600

    update to VS2026

---
+++ Web.API.Learning/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+// Add these two lines for Swagger/OpenAPI support
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        // In .NET 10+, the default endpoint URL has changed
+        options.SwaggerEndpoint("/openapi/v1.json", "My API V1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
